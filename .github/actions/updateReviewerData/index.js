c193fde34d96b76ba808187e72c7ea6f9cde4770
Zmievskii <zmievsky@uchi.ru>
2021-03-31 19:09:53 +0300

    add reset

--- .github/actions/updateReviewerData/index.js
+++ .github/actions/updateReviewerData/index.js
@@ -1,11 +1,17 @@
 import fetch from 'node-fetch';
 import * as core from '@actions/core';
 
+const modes = {
+  add: 'add',
+  reset: 'reset',
+};
+
 async function updateReviewerData() {
   try {
     const storageId = core.getInput('storageId');
     const storageKey = core.getInput('storageToken');
     const name = core.getInput('name');
+    const mode = core.getInput('mode');
     const headers = {
       'Content-Type': 'application/json',
       'X-Master-Key': storageKey,
@@ -17,10 +23,19 @@ async function updateReviewerData() {
     const {
       record: { reviewers },
     } = await reviewersData.json();
-    const index = reviewers.findIndex((reviewer) => reviewer.name === name);
-    reviewers[index].count += 1;
-    const updatedData = { reviewers: [...reviewers] };
-    console.log(name)
+
+    let updatedData = reviewers;
+    if (mode === modes.add) {
+      const index = reviewers.findIndex((reviewer) => reviewer.name === name);
+      reviewers[index].count += 1;
+      updatedData = { reviewers: [...reviewers] };
+    } else if (mode === modes.reset) {
+      updatedData = reviewers.map((reviewer, index) => {
+        reviewers[index].count = 0;
+        return reviewer;
+      });
+    }
+
     await fetch(`https://api.jsonbin.io/v3/b/${storageId}`, {
       method: 'PUT',
       body: JSON.stringify(updatedData),
